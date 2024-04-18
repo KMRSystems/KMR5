360a26fc7cfca4f128787ded36729029f5dd838e
KMRSystems <166872443+KMRSystems@users.noreply.github.com>
2024-04-18 18:24:55 +0300

    Add file

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'KMR Core 5',
+  'description' => 'KMR Core 5',
+  'category' => 'plugin',
+  'version' => '5.0.0',
+  'state' => 'stable',
+  'clearcacheonload' => 0,
+  'author' => 'KMR Systems',
+  'author_email' => 'office@kmr.systems',
+  'author_company' => 'KMR Systems',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '12.0.0-12.4.99',
+      'php' => '8.0.0-8.2.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
